f901345984edbe8b61395f743476f19f74ed8c94
Average-user <lucaspolymeris@protonmail.com>
2020-07-30 12:46:42 -0400

    bound=200 as new "standard"

--- test.sh
+++ test.sh
@@ -6,15 +6,15 @@ NC='\033[0m'
 
 testdim()
 {
-    OUT=$(luajit main.lua $1 $2 100)
-    difference=$(diff $1-$2-100.txt test-outputs/$1-$2-100.txt)
+    OUT=$(luajit main.lua $1 $2 200)
+    difference=$(diff $1-$2-200.txt test-outputs/$1-$2-200.txt)
     if [ "${difference}" != "" ]
     then
         printf "${RED}X${NC} ${OUT}\n"
     else
         printf "${GREEN}O${NC} ${OUT}\n"
     fi
-    rm $1-$2-100.txt
+    rm $1-$2-200.txt
 }
 
 testdim 2 2
@@ -22,4 +22,3 @@ testdim 3 2
 testdim 2 3
 testdim 4 2
 testdim 2 4
-
